4c35bd11c1c07d6af623176da4667d2123375fd0
Riverwindli <lwy23czl@163.com>
2023-04-03 23:54:31 +0800

    在字符流中匹配出图片url进行保存

--- src/main/java/cn/river/FileHandle.java
+++ src/main/java/cn/river/FileHandle.java
@@ -3,15 +3,21 @@ package cn.river;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class FileHandle {
 
     @Value("${directory}")
     private String directory;
+
+    @Value("${urlRegex}")
+    private String regex;
+
     List<String> list =new ArrayList();
 
     /**
@@ -19,6 +25,7 @@ public class FileHandle {
      */
     public List<String> getFileList(){
         File file = new File(directory);
+
         traverseDirectory(file);
         return list;
     }
@@ -36,12 +43,40 @@ public class FileHandle {
                 if (file.isDirectory()) {
                     traverseDirectory(file);
                 } else if (file.isFile() && file.getName().toLowerCase().endsWith(".md")) {
-                    // 处理md格式文件
-                    System.out.println(file.getAbsolutePath());
                     list.add(file.getAbsolutePath());
                 }
             }
         }
         return list;
     }
+
+
+    /**
+     * 根据文件的绝对路径名，遍历获取文件，返回获取每个文件中的图片路径集合
+     */
+    public List<String> getImgUrlList(List<String> fileList) throws IOException {
+        //创建集合
+        List<String> urlList=new ArrayList<>();
+        for (String fileName : fileList) {
+            //创建字符输入流对象
+            FileReader fileReader = new FileReader(fileName);
+            //字符缓冲输入流
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            //读取文件
+            String line;
+            while ((line=bufferedReader.readLine())!=null){
+                Pattern compile = Pattern.compile(regex);
+                Matcher matcher = compile.matcher(line);
+                while (matcher.find()){
+                    String url = matcher.group();
+                    urlList.add(url);
+                }
+            }
+            bufferedReader.close();
+            fileReader.close();
+        }
+        return urlList;
+    }
+
 }
--- src/test/java/cn/river/DrawingBedCleanApplicationTests.java
+++ src/test/java/cn/river/DrawingBedCleanApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.util.List;
+
 @SpringBootTest
 class DrawingBedCleanApplicationTests {
 
@@ -15,8 +18,10 @@ class DrawingBedCleanApplicationTests {
     }
 
     @Test
-    void testFileList(){
-        fileHandle.getFileList();
+    void testFileList() throws IOException {
+        List<String> fileList = fileHandle.getFileList();
+//        System.out.println(fileList);
+        System.out.println(fileHandle.getImgUrlList(fileList));
     }
 
 }
